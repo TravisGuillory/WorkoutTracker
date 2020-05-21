f10f2cf151b3ddf18058bbe52ef09d499bd990cb
Travis Guillory <travisguillory70@gmail.com>
2020-05-20 21:52:50 -0500

    Updated controllers

--- routes/models/workout.js
+++ routes/models/workout.js
@@ -1,43 +1,59 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutSchema =  new Schema(
-    {
-        day: {
-            type: Date, 
-            default: Date.now
-        }, 
-        excercises: [
-            {
-                type:{
-                    type: String, 
-                    trim: true,
-                    required: "Enter excercise type"
-                },
-                name: {
-                    type: String,
-                    trim: true,
-                    required: "Enter the excercise name"
-                },
-                duration: {
-                    type: Nimber,
-                    required: "Enter duration of exercise in minutes"
-                }, 
-                weight: {
-                    type: Number
-                },
-                repetitions: {
-                    type: Number
-                },
-                sets: {
-                    type: Number
-                },
-                distance: {
-                    type: Number
-                }
-            }
-        ]
+const workoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now,
     },
-    
+    excercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Enter excercise type",
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Enter the excercise name",
+        },
+        duration: {
+          type: Nimber,
+          required: "Enter duration of exercise in minutes",
+        },
+        weight: {
+          type: Number,
+        },
+        repetitions: {
+          type: Number,
+        },
+        sets: {
+          type: Number,
+        },
+        distance: {
+          type: Number,
+        },
+      },
+    ],
+  },
+  // Virtual properties when calculated data is needed
+  {
+    toJSON: {
+      virtual: true,
+    },
+  }
 );
+
+// add a Schema property that is dynamically created
+workoutSchema.virtual("totalDuration").get(function () {
+  // Sum the duration of exercices by array.reduce
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercises.duration;
+  }, 0);
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+module.exports = Workout;
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const logger = require('morgan');
-
+const path = require('path');
 const app = express();
 const PORT = preocess.env.PORT || 3000;
 
 
 // Middleware
-app.use(express.urlencoded( { extended: true}));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 app.use(express.static('public'));
 app.use(logger('dev'));
@@ -21,3 +23,8 @@ app.use(require('./routes/htmlRoutes.js'));
 const db = require('./models');
 
 
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.listen(PORT, () => console.log("listening on port: ", PORT));
